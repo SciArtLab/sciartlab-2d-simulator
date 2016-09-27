3225281c0c20818511a02e1bb1dd4a677bd6015e
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-09-27 15:58:07 -0400

    Create Agent class

---
+++ src/com/xmunch/game/model/Agent.groovy
@@ -0,0 +1,56 @@
+package com.xmunch.game.model
+
+import com.xmunch.game.Constants
+
+
+
+public class Agent extends PObject {
+
+    public Agent(){
+        super()
+        x = game.random(0, width) % Constants.AGENT_WIDTH
+        y = game.random(0,height) % Constants.AGENT_HEIGHT
+        width = Constants.AGENT_WIDTH
+        height = Constants.AGENT_HEIGHT
+    }
+
+    void draw() {
+        game.strokeWeight(1)
+        game.stroke(0,235,0)
+        game.fill(0,50,0)
+        game.rect(x, y, width, height)
+        moveRandomly()
+    }
+
+    void moveRandomly(){
+        def movement = game.random(0,100)
+
+        if(movement <= 25)
+            moveUp()
+        else
+        if(movement <= 50)
+            moveDown()
+        else  if(movement <= 75)
+            moveLeft()
+        else
+        if(movement <= 100)
+            moveRight()
+    }
+
+    void moveUp(){
+        if(y - Constants.AGENT_SPEED > 0)
+            y = y-Constants.AGENT_SPEED
+    }
+    void moveDown(){
+        if(y + Constants.AGENT_SPEED + Constants.AGENT_HEIGHT < global.getSpaceWidth())
+            y = y + Constants.AGENT_SPEED
+    }
+    void moveLeft(){
+        if(x - Constants.AGENT_SPEED > 0)
+            x = x-Constants.AGENT_SPEED
+    }
+    void moveRight(){
+        if(x + Constants.AGENT_SPEED + Constants.AGENT_WIDTH < global.getSpaceHeight())
+            x = x+Constants.AGENT_SPEED
+    }
+}
