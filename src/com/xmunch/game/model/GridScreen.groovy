5c2c6f8a089d9db5bd6bdc752825c3f4fe20927b
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-09-27 15:57:57 -0400

    Changes in GridScreen

--- src/com/xmunch/game/model/GridScreen.groovy
+++ src/com/xmunch/game/model/GridScreen.groovy
@@ -16,7 +16,6 @@ public class GridScreen extends Screen {
     @Override
     void draw() {
         game.background(red, green, blue)
-        int cellIndex = 0
 
         for(int i = 0; i < global.getSpaceWidth(); i = i + Constants.CELL_SIZE){
             for(int j = 0; j < global.getSpaceHeight(); j = j + Constants.CELL_SIZE){
@@ -24,16 +23,14 @@ public class GridScreen extends Screen {
                 game.strokeWeight(0.5)
                 if(i == 0 || i == global.getSpaceWidth() - Constants.CELL_SIZE ||
                 j == 0 || j == global.getSpaceHeight() - Constants.CELL_SIZE ){
-                    game.stroke(0,255,0)
-                    game.fill(0,50,0)
+                    game.stroke(0,255,255)
+                    game.fill(10,50,70)
                 } else {
-                    game.stroke(0,255,0)
+                    game.stroke(0,245,245)
                     game.fill(0,0,0)
                 }
 
-                game.text(i+","+j,i,j)
                 game.rect(i, j, Constants.CELL_SIZE, Constants.CELL_SIZE)
-                cellIndex++
             }
         }
     }
