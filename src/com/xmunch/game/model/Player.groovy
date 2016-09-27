40523411597b9ed6cff4a2fc91c030231bcbfa06
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-09-27 15:58:19 -0400

    Extend Player from agent

--- src/com/xmunch/game/model/Player.groovy
+++ src/com/xmunch/game/model/Player.groovy
@@ -5,37 +5,20 @@ import com.xmunch.game.GlobalSpace
 
 
 
-public class Player extends PObject {
+public class Player extends Agent {
 
     public Player(){
         super()
         x = (float)global.getSpaceWidth()/2
         y = (float)global.getSpaceHeight()/2
-        width = Constants.PLAYER_WIDTH
-        height = Constants.PLAYER_HEIGHT
+        width = Constants.AGENT_WIDTH
+        height = Constants.AGENT_HEIGHT
     }
 
     void draw() {
         game.strokeWeight(1)
-        game.stroke(0,255,0)
-        game.fill(0,50,0)
+        game.stroke(100,110,255)
+        game.fill(50,60,60)
         game.rect(x,y,width,height)
-    }
-
-    void moveUp(){
-        if(y - Constants.PLAYER_SPEED > 0)
-            y = y-Constants.PLAYER_SPEED
-    }
-    void moveDown(){
-        if(y + Constants.PLAYER_SPEED + Constants.PLAYER_HEIGHT < global.getSpaceWidth())
-            y = y + Constants.PLAYER_SPEED
-    }
-    void moveLeft(){
-        if(x - Constants.PLAYER_SPEED > 0)
-            x = x-Constants.PLAYER_SPEED
-    }
-    void moveRight(){
-        if(x + Constants.PLAYER_SPEED + Constants.PLAYER_WIDTH < global.getSpaceHeight())
-            x = x+Constants.PLAYER_SPEED
     }
 }
