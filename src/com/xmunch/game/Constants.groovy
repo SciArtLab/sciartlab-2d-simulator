fe6e65407f849aded5b42506c836e6a8bdf3c6ab
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-09-28 09:44:32 -0400

    Add new constants

--- src/com/xmunch/game/Constants.groovy
+++ src/com/xmunch/game/Constants.groovy
@@ -6,18 +6,18 @@ class Constants {
     public final static boolean FULL_SCREEN_DEFAULT = false //todo: ver que pasa con los buenos
     public final static int HEIGHT = 700
     public final static int WIDTH = 700
-    public final static int OBJECT_SIZE = 50
+    public final static int OBJECT_SIZE = 100
     public final static float FRAME_RATE = 10
     public final static DEFAULT_CUR_MSG = "..."
     public final static CELL_SIZE = OBJECT_SIZE
 
     //Player configuration
-    public final static int AGENT_SPEED = OBJECT_SIZE
+    public final static int AGENT_SPEED = OBJECT_SIZE/5
     public final static int AGENT_WIDTH = OBJECT_SIZE
     public final static int AGENT_HEIGHT = OBJECT_SIZE
 
     //Agents configuration
-    public final static int AGENTS_NUMBER = 10
+    public final static int AGENTS_NUMBER = 20
     public final static float NEIGHBOR_DISTANCE = 3*OBJECT_SIZE
 
     //Keys codes
